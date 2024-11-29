209f9b11e8c0a6447a0bd5d4b791c4b1f5f44972
hediye-ersan <hediyesnl9@gmail.com>
2024-11-29 23:55:34 +0300

    dark mod için context oluşturuldu.

---
+++ src/contexts/DarkModeContext.jsx
@@ -0,0 +1,20 @@
+import React, { createContext, useState, useContext } from 'react';
+
+export const DarkModeContext = createContext();
+
+export const DarkModeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false); 
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prev) => !prev); 
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+
+export const useDarkMode = () => useContext(DarkModeContext);//custom hook ekledim.
