bc437e5308086db63524ec5f2ec8445c863c45f2
hediye-ersan <hediyesnl9@gmail.com>
2024-11-30 02:01:29 +0300

    app.jsx düzenlendi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { LanguageProvider, useLanguage } from './contexts/LanguageContext';
+import { DarkModeProvider, useDarkMode } from './contexts/DarkModeContext';
+import './App.css'
+import Header from './components/Header';
+import Skills from './components/Skills'
+import myData from './data/myData.json';
+
+const LanguageToggleButton = () => {
+  const { language, toggleLanguage } = useLanguage();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h1 className="text-2xl font-bold underline color-black">
-      Hello world!
-    </h1>
+    <button onClick={toggleLanguage}>
+      {language === 'en' ? 'Türkçeye Çevir' : 'Switch to English'}
+    </button>
+  );
+};
 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const DarkModeToggleButton = () => {
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
+
+  return (
+    <button onClick={toggleDarkMode}>
+      {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+    </button>
+  );
+};
+
+
+function App() {
+  
+
+  return (
+    
+      <DarkModeProvider>
+        <LanguageProvider>
+          <div>
+          <Header />
+            <Skills/>
+          </div>
+        </LanguageProvider>
+      </DarkModeProvider>
+    
   )
 }
 
