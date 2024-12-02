d31d90db81d3a41233145918085927e6d36fb2ec
hediye-ersan <hediyesnl9@gmail.com>
2024-12-02 17:02:17 +0300

    Skills ve Projects iççin componentler oluşturuldu.

---
+++ src/components/Projects.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Projects = ({ projectsData }) => {
+  return (
+    <section className="m-24 ">
+      <h2 className="text-4xl font-medium font-inter ">{projectsData.title}</h2>
+
+      <div className='flex gap-8'>
+        {/* First Project */}
+        <div className="mt-12 w-2/4 text-left px-12 py-12 bg-[#DDEEFE] relative rounded-xl">
+          <h3 className="text-[29px] font-bold font-playfair">{projectsData.firstProject.title}</h3>
+          <p className='text-base font-inter'>{projectsData.firstProject.text}</p>
+          <div className="flex gap-2 flex-wrap w-3/4 py-2 ">
+            {Object.values(projectsData.firstProject.skills).map((skill, index) => (
+              <p key={index} className="font-playfair font-bold text-base border bg-[#FFFFFF] p-2 px-6 rounded-3xl">{skill}</p>
+            ))}
+          </div>
+          <div className="flex justify-between pt-6">
+
+            <p className='font-semibold font-inter text-xl'>{projectsData.firstProject.viewGit.text}</p>
+            <button className='font-semibold font-inter text-xl'>{projectsData.firstProject.viewGit.button}</button>
+          </div>
+          <div className='absolute left-1/2 transform -translate-x-1/2 -bottom-10 w-full'><img src={projectsData.firstProject.image} alt="Project 1" className='m-auto' /></div>
+
+        </div>
+
+        {/* Second Project */}
+        <div className="mt-12 w-2/4 text-left px-12 py-12 bg-[#D9F6F1] relative rounded-xl">
+          <h3 className="text-[29px] font-bold font-playfair">{projectsData.secondProject.title}</h3>
+          <p>{projectsData.secondProject.text}</p>
+          <div className="flex gap-2 flex-wrap w-3/4 py-2 ">
+            {Object.values(projectsData.secondProject.skills).map((skill, index) => (
+              <p key={index} className="font-playfair font-bold text-base border bg-[#FFFFFF] p-2 px-6 rounded-3xl">{skill}</p>
+            ))}
+          </div>
+          <div className="flex justify-between pt-6">
+            <p className='font-semibold font-inter text-xl'>{projectsData.secondProject.viewGit.text}</p>
+            <button className='font-semibold font-inter text-xl'>{projectsData.secondProject.viewGit.button}</button>
+          </div>
+          <div>
+            <img src={projectsData.secondProject.image} alt="Project 2" className='m-auto ' /></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
-const Projects = () => {
+const Skills = ({ skillsData }) => {
   return (
-    <section id="projects" className="projects">
-      <h2>My Projects</h2>
-      <div className="project-list">
-        <div className="project">
-          <h3>Project 1</h3>
-          <p>Project 1 description...</p>
-        </div>
-        {/* Diğer projelerinizi buraya ekleyebilirsiniz */}
+    <section className="py-16 font-inter">
+      <h2 className=" text-5xl ">{skillsData.title}</h2>
+      <div className="flex flex-wrap gap-8 mt-12 mb-12 justify-center text-2xl">
+        {skillsData.skill.map((skill, index) => (
+          <div key={index} className="text-center">
+            <img
+              src={skill.image}
+              alt={skill.name}
+              className="mx-auto mb-2"
+            />
+            <p className='text-[#777777]'>{skill.name}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
-};
+}
 
-export default Projects;
+export default Skills;
