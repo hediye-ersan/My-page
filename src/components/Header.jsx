bcbf31c3bd429f1ec40ad77a6199dc7be483e4ce
hediye-ersan <hediyesnl9@gmail.com>
2024-12-02 17:01:35 +0300

    Header componenti oluşturuldu.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import "../reset.css"
 
-const Header = () => {
+const Header = ({ bioData }) => {
   return (
-    <header className="header">
-      <h1>Welcome to My Portfolio</h1>
-      {/* Burada başlık, logo ya da gezinme bağlantıları ekleyebilirsiniz */}
-    </header>
+    <section className="bg-[#F4F4F4] font-inter text-left px-28 py-24 z-10">
+
+      <header>
+        <div>
+          <h1 className='font-inter text-3xl'>{bioData.title}</h1>
+          <div className="flex justify-center ">
+            <p className='text-[42px] max-w-[14]'>{bioData.text}</p>
+            <img src={bioData.image} alt="Profile" />
+          </div>
+        </div>
+        <article className='text-lg'>
+          <figure className='flex gap-6'>
+            <img src={bioData.logo1} alt="Github" />
+            <img src={bioData.logo2} alt="Linkedin" />
+          </figure>
+          <p className='text-lg pt-8 w-2/4'>{bioData.intro}</p>
+        </article>
+      </header>
+    </section>
   );
-};
+}
 
 export default Header;
